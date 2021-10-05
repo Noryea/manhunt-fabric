d4e1e8db8c5c78b510713a60aff0ec0fb855b007
Noryea <88097529+Noryea@users.noreply.github.com>
2021-10-05 20:17:56 +0800

    Add Command Registration

--- src/main/java/cn/noryea/manhunt/Main.java
+++ src/main/java/cn/noryea/manhunt/Main.java
@@ -1,9 +1,11 @@
 package cn.noryea.manhunt;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.Team;
+import net.minecraft.util.Formatting;
 
 public class Main implements ModInitializer {
     @Override
@@ -14,12 +16,18 @@ public class Main implements ModInitializer {
             Scoreboard scoreboard = world.getServer().getScoreboard();
             if (scoreboard.getTeam("hunters") == null) {
                 Team team = scoreboard.addTeam("hunters");
+                team.setColor(Formatting.DARK_AQUA);
             }
 
             if (scoreboard.getTeam("runners") == null) {
                 Team team= scoreboard.addTeam("runners");
+                team.setColor(Formatting.RED);
             }
         });
 
+        //命令注册
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            ManhuntCommand.registerCommands(dispatcher);
+        });
     }
 }
