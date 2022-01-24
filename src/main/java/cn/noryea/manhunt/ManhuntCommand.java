0dc6ec6e1e891e8b3e41cd91e049669b792b07dc
Noryea <88097529+Noryea@users.noreply.github.com>
2022-01-24 15:49:54 +0800

    Add command to freeze hunters.

--- src/main/java/cn/noryea/manhunt/ManhuntCommand.java
+++ src/main/java/cn/noryea/manhunt/ManhuntCommand.java
@@ -1,62 +1,142 @@
-package cn.noryea.manhunt;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+package net.minecraft.server.command;
+
+import com.google.common.collect.ImmutableList;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.command.argument.TeamArgumentType;
-import net.minecraft.entity.Entity;
-import net.minecraft.scoreboard.Scoreboard;
-import net.minecraft.scoreboard.Team;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.TranslatableText;
-
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import java.util.Collection;
 import java.util.Iterator;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.StatusEffectArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.text.TranslatableText;
+import org.jetbrains.annotations.Nullable;
 
-public class ManhuntCommand {
-    public static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
+public class EffectCommand {
+    private static final SimpleCommandExceptionType GIVE_FAILED_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("commands.effect.give.failed"));
+    private static final SimpleCommandExceptionType CLEAR_EVERYTHING_FAILED_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("commands.effect.clear.everything.failed"));
+    private static final SimpleCommandExceptionType CLEAR_SPECIFIC_FAILED_EXCEPTION = new SimpleCommandExceptionType(new TranslatableText("commands.effect.clear.specific.failed"));
 
-        dispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder)(LiteralArgumentBuilder)((LiteralArgumentBuilder)CommandManager.literal("mh").requires((source) -> {
-            return source.hasPermissionLevel(0);
-        })).then(CommandManager.literal("join").then(CommandManager.argument("team", TeamArgumentType.team()).executes((context) -> {
-            return executeJoin((ServerCommandSource)context.getSource(), TeamArgumentType.getTeam(context, "team"));
-        })))).then(CommandManager.literal("cure").then(CommandManager.argument("targets", EntityArgumentType.players()).executes((context) -> {
-            return executeCure((ServerCommandSource)context.getSource(), EntityArgumentType.getPlayers(context, "targets"));
-        }))));
-
+    public EffectCommand() {
     }
 
-    private static int executeJoin(ServerCommandSource source, Team team) {
-        Scoreboard scoreboard = source.getServer().getScoreboard();
-
-        scoreboard.addPlayerToTeam(source.getEntity().getName().asString(), team);
-        source.sendFeedback(new TranslatableText("commands.team.join.success.single", source.getEntity().getName(), team.getFormattedName()), true);
-
-        return 1;
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder)((LiteralArgumentBuilder)CommandManager.literal("effect").requires((source) -> {
+            return source.hasPermissionLevel(2);
+        })).then(((LiteralArgumentBuilder)CommandManager.literal("clear").executes((context) -> {
+            return executeClear((ServerCommandSource)context.getSource(), ImmutableList.of(((ServerCommandSource)context.getSource()).getEntityOrThrow()));
+        })).then(((RequiredArgumentBuilder)CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> {
+            return executeClear((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"));
+        })).then(CommandManager.argument("effect", StatusEffectArgumentType.statusEffect()).executes((context) -> {
+            return executeClear((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"), StatusEffectArgumentType.getStatusEffect(context, "effect"));
+        }))))).then(CommandManager.literal("give").then(CommandManager.argument("targets", EntityArgumentType.entities()).then(((RequiredArgumentBuilder)CommandManager.argument("effect", StatusEffectArgumentType.statusEffect()).executes((context) -> {
+            return executeGive((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"), StatusEffectArgumentType.getStatusEffect(context, "effect"), (Integer)null, 0, true);
+        })).then(((RequiredArgumentBuilder)CommandManager.argument("seconds", IntegerArgumentType.integer(1, 1000000)).executes((context) -> {
+            return executeGive((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"), StatusEffectArgumentType.getStatusEffect(context, "effect"), IntegerArgumentType.getInteger(context, "seconds"), 0, true);
+        })).then(((RequiredArgumentBuilder)CommandManager.argument("amplifier", IntegerArgumentType.integer(0, 255)).executes((context) -> {
+            return executeGive((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"), StatusEffectArgumentType.getStatusEffect(context, "effect"), IntegerArgumentType.getInteger(context, "seconds"), IntegerArgumentType.getInteger(context, "amplifier"), true);
+        })).then(CommandManager.argument("hideParticles", BoolArgumentType.bool()).executes((context) -> {
+            return executeGive((ServerCommandSource)context.getSource(), EntityArgumentType.getEntities(context, "targets"), StatusEffectArgumentType.getStatusEffect(context, "effect"), IntegerArgumentType.getInteger(context, "seconds"), IntegerArgumentType.getInteger(context, "amplifier"), !BoolArgumentType.getBool(context, "hideParticles"));
+        }))))))));
     }
 
-    private static int executeCure(ServerCommandSource source, Collection<? extends Entity> targets) {
-        if (source.hasPermissionLevel(2)) {
-            Iterator var3 = targets.iterator();
-
-            while (var3.hasNext()) {
-                ServerPlayerEntity player = (ServerPlayerEntity) var3.next();
-
-                player.clearStatusEffects();
-                player.setHealth(player.getMaxHealth());
-                player.getHungerManager().setFoodLevel(20);
-                player.getHungerManager().setSaturationLevel(8.5F);
-
+    private static int executeGive(ServerCommandSource source, Collection<? extends Entity> targets, StatusEffect effect, @Nullable Integer seconds, int amplifier, boolean showParticles) throws CommandSyntaxException {
+        int i = 0;
+        int j;
+        if (seconds != null) {
+            if (effect.isInstant()) {
+                j = seconds;
+            } else {
+                j = seconds * 20;
             }
-            source.sendFeedback(new LiteralText("已治愈" + targets.size() + "名玩家"), true);
-            return targets.size();
-
+        } else if (effect.isInstant()) {
+            j = 1;
         } else {
-            source.sendFeedback(new LiteralText("\u00a7c宁不配"), false);
-            return 0;
+            j = 600;
+        }
+
+        Iterator var8 = targets.iterator();
+
+        while(var8.hasNext()) {
+            Entity entity = (Entity)var8.next();
+            if (entity instanceof LivingEntity) {
+                StatusEffectInstance statusEffectInstance = new StatusEffectInstance(effect, j, amplifier, false, showParticles);
+                if (((LivingEntity)entity).addStatusEffect(statusEffectInstance, source.getEntity())) {
+                    ++i;
+                }
+            }
+        }
+
+        if (i == 0) {
+            throw GIVE_FAILED_EXCEPTION.create();
+        } else {
+            if (targets.size() == 1) {
+                source.sendFeedback(new TranslatableText("commands.effect.give.success.single", new Object[]{effect.getName(), ((Entity)targets.iterator().next()).getDisplayName(), j / 20}), true);
+            } else {
+                source.sendFeedback(new TranslatableText("commands.effect.give.success.multiple", new Object[]{effect.getName(), targets.size(), j / 20}), true);
+            }
+
+            return i;
+        }
+    }
+
+    private static int executeClear(ServerCommandSource source, Collection<? extends Entity> targets) throws CommandSyntaxException {
+        int i = 0;
+        Iterator var3 = targets.iterator();
+
+        while(var3.hasNext()) {
+            Entity entity = (Entity)var3.next();
+            if (entity instanceof LivingEntity && ((LivingEntity)entity).clearStatusEffects()) {
+                ++i;
+            }
+        }
+
+        if (i == 0) {
+            throw CLEAR_EVERYTHING_FAILED_EXCEPTION.create();
+        } else {
+            if (targets.size() == 1) {
+                source.sendFeedback(new TranslatableText("commands.effect.clear.everything.success.single", new Object[]{((Entity)targets.iterator().next()).getDisplayName()}), true);
+            } else {
+                source.sendFeedback(new TranslatableText("commands.effect.clear.everything.success.multiple", new Object[]{targets.size()}), true);
+            }
+
+            return i;
+        }
+    }
+
+    private static int executeClear(ServerCommandSource source, Collection<? extends Entity> targets, StatusEffect effect) throws CommandSyntaxException {
+        int i = 0;
+        Iterator var4 = targets.iterator();
+
+        while(var4.hasNext()) {
+            Entity entity = (Entity)var4.next();
+            if (entity instanceof LivingEntity && ((LivingEntity)entity).removeStatusEffect(effect)) {
+                ++i;
+            }
+        }
+
+        if (i == 0) {
+            throw CLEAR_SPECIFIC_FAILED_EXCEPTION.create();
+        } else {
+            if (targets.size() == 1) {
+                source.sendFeedback(new TranslatableText("commands.effect.clear.specific.success.single", new Object[]{effect.getName(), ((Entity)targets.iterator().next()).getDisplayName()}), true);
+            } else {
+                source.sendFeedback(new TranslatableText("commands.effect.clear.specific.success.multiple", new Object[]{effect.getName(), targets.size()}), true);
+            }
+
+            return i;
         }
     }
 }
